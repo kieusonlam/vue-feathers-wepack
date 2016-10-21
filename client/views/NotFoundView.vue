d0c39d143b519bbaced09d5b379584b7290e0c1c
Lam Kieu <rainbow.lam25@gmail.com>
2016-10-21 09:14:41 +0700

    Initial commit

---
+++ client/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="user-view">
+    <h1>Page Not Found!</h1>
+  </div>
+</template>
+
+<style lang="stylus">
+.user-view
+  background-color #fff
+  box-sizing border-box
+  padding 2em 3em
+</style>
---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const app = require('./app');
+const api = require('./api');
+const port = app.get('port');
+const server = app.listen(port);
+api.setup(server);
+
+server.on('listening', () =>
+  console.log(`Feathers application started on ${app.get('host')}:${port}`)
+);
